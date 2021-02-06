8246c8908b609742417a08d082d2db71b969ce90
Jaffar Bashir <jaffarbashir786@gmail.com>
2021-02-06 23:19:02 +0530

    Wrote some programs on Strings

---
+++ Lists/concatination_list.py
@@ -0,0 +1,6 @@
+"""Lists are ordered sequence"""
+# Concatination of lists
+list_one = [1,2,3,4,5]
+list_two = ["one",'two','three','four','five']
+con = list_one + list_two
+print(f"{con}")
---
+++ Lists/list_methods.py
@@ -0,0 +1,7 @@
+#Python various methods on lists
+list_one = [1,3,43,32,544,67,898,89]
+print(list_one.pop()) #removes the last element.
+print(list_one.append(8)) #add the element at the end.
+print(sorted(list_one))
+print(list_one.insert(2,400))
+print(list_one)
